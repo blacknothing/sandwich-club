8ff0e2bf3d9e088df539040e823013bbb87f3d97
Kevin West <kevin.1.west@microsoft.com>
2014-12-12 11:22:15 +0000

    Initial commit
    Empty Xamarin Forms app

---
+++ Sandwich-Club-App/App.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace SandwichClubApp
+{
+    public class App
+    {
+        public static Page GetMainPage()
+        {	
+            return new ContentPage
+            { 
+                Content = new Label
+                {
+                    Text = "Hello, Sandwich Club!",
+                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                },
+            };
+        }
+    }
+}
+
